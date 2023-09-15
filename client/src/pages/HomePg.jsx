aab91d05b7d1192af6b0cbc9a905dea6dd7cf7b9
rimi <a.fatimaarimi@gmail.com>
2023-09-15 15:58:07 -0400

    renders authpg

---
+++ client/src/pages/HomePg.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import NavBar from '../components/NavBar'
+// import AuthPg from './AuthPg'
+
+const HomePg = () => {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  )
+}
+
+export default HomePg
