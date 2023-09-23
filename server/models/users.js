1c86475de9fa4d4605bf1170f05ea1423cbfb113
rimi <a.fatimaarimi@gmail.com>
2023-09-23 19:47:04 -0400

    added timestamp to model

--- server/models/users.js
+++ server/models/users.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minLength: 6,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    minLength: 6,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const UserModel = mongoose.model("User", userSchema);
 module.exports = UserModel;
