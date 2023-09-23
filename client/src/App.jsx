c11e6968119afd86f8604c9063fd44c50ac98c8e
rimi <a.fatimaarimi@gmail.com>
2023-09-23 19:45:12 -0400

    removed condition for homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <>
-    <Toaster position="bottom-center" toastOptions={{duration: 2000}}/>
+      <Toaster position="bottom-center" toastOptions={{ duration: 2000 }} />
       <Router>
         {/* Define the routes for your application */}
         <Routes>
@@ -31,7 +31,7 @@ function App() {
           <Route path="/SignUp" element={<SignUp />} />
 
           {/* Route for the Home component, conditionally rendering either Home or AuthPg */}
-          <Route path="/Home" element={user ? homePage : authPage} />
+          <Route path="/Home" element={<HomePg />} />
 
           {/* Fallback route for the root URL "/", also conditionally rendering Home or AuthPg */}
           <Route path="/" element={user ? homePage : authPage} />
