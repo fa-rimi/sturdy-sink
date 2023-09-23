9f05bf4becad1adec458d07c280ec72e7ac2dc59
rimi <a.fatimaarimi@gmail.com>
2023-09-23 19:46:32 -0400

    created router.post for user login

--- server/routes/authAPI.js
+++ server/routes/authAPI.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { test, registerUser } = require('../controllers/authControllers');
+const { test, registerUser, loginUser } = require('../controllers/authControllers');
 
 // Routes
 router.get("/", test)
 router.post("/SignUp", registerUser)
+router.post("/SignIn", loginUser)
 
 // Export
 module.exports = router;
