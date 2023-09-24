7ddc0941ae206b70e8d5811fb3fda4e69dc44aa1
rimi <a.fatimaarimi@gmail.com>
2023-09-24 03:01:50 -0400

    pseudo routes

---
+++ server/routes/dictionaryAPI.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Dictionary Routes
+// Get: See All Words
+// Post: Create Word 
+// router.post("/AddNewWord", newEntry)
+// Post: Update Word
+// Delete: Delete Word
+
+module.exports = router;
