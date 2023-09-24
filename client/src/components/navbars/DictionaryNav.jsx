65a788bb1e6e1051a9f95ed9904d6c3cbe900c73
rimi <a.fatimaarimi@gmail.com>
2023-09-24 03:03:47 -0400

    new navbar components for dictionary, logout button functional

---
+++ client/src/components/navbars/DictionaryNav.jsx
@@ -0,0 +1,15 @@
+const DictionaryNav = () => {
+  return (
+    <nav className="w-[100vw] h-[40px] bg-black flex justify-between items-center">
+        <div>
+            {/* Hamburger Menu */}
+        </div>
+        <div>
+            {/* Search Bar */}
+            <input type="search" id="search" />
+        </div>
+    </nav>
+  )
+}
+
+export default DictionaryNav
---
+++ client/src/components/navbars/MainNav.jsx
@@ -0,0 +1,17 @@
+import { ImBook } from "react-icons/im";
+import { Link } from "react-router-dom";
+import LogoutBtn from "../LogoutBtn";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <Link to="/Dictionary">
+        <ImBook />
+      </Link>
+
+      <LogoutBtn/>
+    </nav>
+  );
+};
+
+export default NavBar;
